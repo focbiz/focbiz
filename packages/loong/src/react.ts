5d4a13ee2a69ed1c16f9b4574c0c9875900e27a5
sprint-zhang <649177782@qq.com>
2024-04-15 17:17:31 +0800

    fix:not clean list

--- packages/loong/src/react.ts
+++ packages/loong/src/react.ts
@@ -4,14 +4,15 @@ export function useLoong<T>(hander: any) {
     const newData = useSyncExternalStore((cb: Function) => {
         hander.loongEr.subscribe(cb)
         return () => {
-            hander.chamber.clansman.delete(hander.loongEr.ryuko)
         }
     }, () => {
         return hander.chamber.message.get(hander.loongRoar.ryuko)
     }, () => { })
 
-    return [newData, (a: Object) => {
+    return [newData, (a: Object, isCloseNotify: boolean) => {
         hander.loongRoar.publish(a)
-        hander.chamber.notify()
-    }] as [T, (a: T) => void]
+        if (!isCloseNotify) {
+            hander.chamber.notify()
+        }
+    }] as [T, (a: T, isCloseNotify?: boolean) => void]
 }
